bb280dc6e5c34029c88ed12362df41691de54e08
KyeongHoSeong <dasomaru@gmail.com>
2020-08-31 06:10:32 +0900

    step 5

--- functions/index.js
+++ functions/index.js
@@ -9,33 +9,32 @@ const app = express();
 
 app.get('/screams', (req, res) => {
     admin
-    .firestore().collection('screams').get()
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
         .then(data => {
             let screams = [];
             data.forEach(doc=> {
-                screams.push(doc.data());
+                //screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return res.json(screams);
         })
         .catch(err => console.error(err));
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello World!");
-});
- 
-// exports.getScreams = functions.https.onRequest((req,res) => {
-    
-// })
-
-exports.createScream = functions.https.onRequest((req,res) => {
-    if(req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed' })
-    }
+app.post('/scream', (req,res) => {  
     const newScreams = {
         body:req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        //createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin.firestore()
@@ -50,5 +49,38 @@ exports.createScream = functions.https.onRequest((req,res) => {
         });
 });
 
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello World!");
+});
+ 
+// exports.getScreams = functions.https.onRequest((req,res) => {
+    
+// })
+
+// exports.createScream = functions.https.onRequest((req,res) => {
+//     if(req.method !== 'POST') {
+//         return res.status(400).json({ error: 'Method not allowed' })
+//     }
+//     const newScreams = {
+//         body:req.body.body,
+//         userHandle: req.body.userHandle,
+//         createdAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+
+//     admin.firestore()
+//         .collection('screams')
+//         .add(newScreams)
+//         .then(doc => {
+//             res.json({messabe: `document ${doc.id} created successfully`});
+//         })
+//         .catch(err=> {
+//             res.status(500).json({eror: 'something went wrong' });
+//             console.error(err);
+//         });
+// });
+
 // https://baseurl.com/api/
-exports.api = functions.https.onRequest(app);
+//exports.api = functions.https.onRequest(app); 
+exports.api = functions.region('asia-northeast3').https.onRequest(app); 
+
+
