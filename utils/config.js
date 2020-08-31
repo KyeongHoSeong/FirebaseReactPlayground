148e13db8409e0c996468303fcec288ab97610bd
KyeongHoSeong <dasomaru@gmail.com>
2020-08-31 18:47:19 +0900

    Profile Image Upload

---
+++ utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAf3mgT3HiJAhGLuT9HN80A3E0fp-nTe9M",
+    authDomain: "fireactplayground.firebaseapp.com",
+    databaseURL: "https://fireactplayground.firebaseio.com",
+    projectId: "fireactplayground",
+    storageBucket: "fireactplayground.appspot.com",
+    messagingSenderId: "860050537620",
+    appId: "1:860050537620:web:d1d568049934984a06c881",
+    measurementId: "G-LZH00LGQEY"
+  };
